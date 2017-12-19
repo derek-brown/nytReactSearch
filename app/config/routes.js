44278e49b261c95d9d9706034d2411ffc5834cbc
Derek Brown <derek.ethan.brown@gmail.com>
2017-12-18 23:04:04 -0500

    commit

---
+++ app/config/routes.js
@@ -0,0 +1,24 @@
+
+var React = require('react');
+
+var Main = require('../components/Main');
+var Saved = require('../components/Saved');
+var Search = require('../components/Search.js');
+
+var Router = require('react-router');
+var Route = Router.Route;
+
+var IndexRoute = Router.IndexRoute;
+
+
+
+//export the routes
+module.exports = (
+	<Route path="/" component={Main}>
+{/*if the user selects search or save, show the appropriate component */}
+		<Route path="/search" component={Search} />
+		<Route path="/saved" component={Saved} />
+{/*if the user selects any other path .. it takes them home */}
+		<IndexRoute component={Search} />
+	</Route>
+);
